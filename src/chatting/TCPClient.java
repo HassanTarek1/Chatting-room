57abcb53281ac10e90613bd1485f3cc8313f76c3
HassanTarek1 <47870697+HassanTarek1@users.noreply.github.com>
2019-04-07 21:01:59 +0200

    changed to printwriter

--- src/chatting/TCPClient.java
+++ src/chatting/TCPClient.java
@@ -12,11 +12,11 @@ public class TCPClient {
 		String sentence;
 		String modifiedSentence;
 		BufferedReader inFromUser=new BufferedReader(new InputStreamReader(System.in));
-		Socket clientSocket = new Socket(address, 5855);
-		DataOutputStream outToServer=new DataOutputStream(clientSocket.getOutputStream());
+		Socket clientSocket = new Socket(address, 5830);
+		PrintWriter outToServer=new PrintWriter(clientSocket.getOutputStream());
 		BufferedReader inFromServer =new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
 		sentence=inFromUser.readLine();
-		outToServer.writeBytes(sentence+"\n");
+		outToServer.write(sentence+"\n");
 		modifiedSentence=inFromServer.readLine();
 		System.out.println("From Server : "+modifiedSentence);
 		clientSocket.close();
--- src/chatting/TCPServer.java
+++ src/chatting/TCPServer.java
@@ -7,14 +7,14 @@ public class TCPServer {
 	public static void main(String[] args) throws Exception {
 		String clientSentence;
 		String capitalizedSentence;
-		ServerSocket welcomeSocket=new ServerSocket(585);
+		ServerSocket welcomeSocket=new ServerSocket(5830);
 		while(true) {
 			Socket connectionSocket=welcomeSocket.accept();
 			BufferedReader inFromClient=new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
-			DataOutputStream outToClient=new DataOutputStream(connectionSocket.getOutputStream());
+			PrintWriter outToClient=new PrintWriter(connectionSocket.getOutputStream());
 			clientSentence=inFromClient.readLine();
 			capitalizedSentence=clientSentence.toUpperCase();
-			outToClient.writeBytes(capitalizedSentence);
+			outToClient.write(capitalizedSentence);
 		}
 	}
 

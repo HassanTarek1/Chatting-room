5f80e15ebdca9d45354ec5ce5fbd88fe27d12b8a
HassanTarek1 <47870697+HassanTarek1@users.noreply.github.com>
2019-10-14 16:20:16 +0200

    no change

---
+++ src/Milestone1/Server.java
@@ -0,0 +1,26 @@
+package Milestone1;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+
+public class Server {
+
+	public static void main(String[] args) throws Exception {
+		ServerSocket welcomeSocket=new ServerSocket(5000);
+		String clientSentence = "";
+		String capitalisedSentence;
+		ArrayList<ClientThread> arr = new ArrayList<ClientThread>();
+		System.out.println("Waiting for connection");
+		while(true) {
+			Socket connectionSocket=welcomeSocket.accept();
+			ClientThread client = new ClientThread(connectionSocket);
+			client.start();
+			arr.add(client);
+		}
+		
+	}
+}
